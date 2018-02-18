fcfb44fac1a427363d7949329efec24eb9cab8ee
David THENON <sveetch@gmail.com>
2018-02-18 15:01:24 +0100

    Forgotted to add migrations, stand as 0.3.0

---
+++ cmsplugin_blocks/choices_helpers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+
+def get_card_template_choices():
+    return settings.BLOCKS_CARD_TEMPLATES
+
+
+def get_card_default_template():
+    return settings.BLOCKS_CARD_TEMPLATES[0][0]
+
+
+def get_hero_template_choices():
+    return settings.BLOCKS_HERO_TEMPLATES
+
+
+def get_hero_default_template():
+    return settings.BLOCKS_HERO_TEMPLATES[0][0]
+
+
+def get_slider_template_choices():
+    return settings.BLOCKS_SLIDER_TEMPLATES
+
+
+def get_slider_default_template():
+    return settings.BLOCKS_SLIDER_TEMPLATES[0][0]
