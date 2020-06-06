789a8de53e2bb2f5d28ef293247ce929d72f123a
David THENON <sveetch@gmail.com>
2020-06-06 17:51:58 +0200

    Yet another attempt to fix documentation build with RTD

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# Use index.rst as root content doc file
+master_doc = 'index'
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
